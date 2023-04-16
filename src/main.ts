6b71dd4342f6af564628b93983f5d2dd420b584c
Dione <dioneousseynou03@gmail.com>
2023-04-16 23:50:21 +0000

    activation CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();  //Activation du CORS
   await app.listen(3000);
 }
 bootstrap();
